c05fa891c348fcd88b2b7f6507e71ee67366623e
Mesele Assefa <getmesy@gmail.com>
2023-08-02 17:58:44 +0300

    pycodestyle

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -10,7 +10,7 @@ babel = Babel(app)
 class Config:
     """ config class """
     LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'    
+    BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
